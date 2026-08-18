5b5d8de76fc5f87dbb182ea7f56ec03906d87990
romansukhachov-cyber <romansukhachov@gmail.com>
2026-08-18 04:28:44 +0100

    Clean up Retrofit dependencies in build.gradle.kts
    Removed commented Retrofit dependencies and redundant implementation lines.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,16 +25,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     
-    // Retrofit для работы с API
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-}dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-
-    // Добавьте вот это:
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
-
